a55bde664abac72b7014e2c184ea82a993a186a9
lch <407410113@qq.com>
2019-04-03 10:08:58 +0800

    xiugai

--- automation_demo/pageobjects/baidu_news_home.py
+++
@@ -1,9 +0,0 @@
-from framework.base_page import BasePage
-
-class NewsHomePage(BasePage):
-    sports_link = "xpath=>//*[@id='channel-shanghai']/div/ul/li[7]/a"
-
-    def click_sports(self):
-        self.click(self.sports_link)
-        self.sleep(2)
-        
